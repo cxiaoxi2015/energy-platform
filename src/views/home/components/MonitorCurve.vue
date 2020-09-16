c9a3cc545920fb60f6abe8673ad1315dc2c8ba42
chenxiaoxi <chenxiaoxi@91isoft.com>
2020-09-16 10:31:10 +0800

    开发监控曲线模块

--- src/views/home/components/MonitorCurve.vue
+++ src/views/home/components/MonitorCurve.vue
@@ -3,12 +3,15 @@
   <div class="monitor-curve">
     <div class="content">
       <div class="title">监控曲线</div>
-      <div class="detail"></div>
+      <div class="detail">
+        <div id="monitorCurve"></div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { Chart } from '@antv/g2'
 export default {
   name: 'MonitorCurve',
   mixins: [],
@@ -16,10 +19,108 @@ export default {
   data () {
     return {}
   },
-  methods: {},
+  methods: {
+    chartInit (data) {
+      const chart = new Chart({
+        container: 'monitorCurve',
+        autoFit: true
+      })
+
+      chart.data(data)
+      chart.scale({
+        hours: {
+          range: [0, 1],
+          tickCount: 25
+        }
+      })
+
+      chart.tooltip({
+        showCrosshairs: true,
+        shared: true,
+        itemTpl: `
+        <div style="margin-bottom: 10px;list-style:none;">
+            <span style="background-color:{color};" class="g2-tooltip-marker"></span>
+            {value}
+        </div>
+      `
+      })
+
+      chart.axis('total', {
+        label: {
+          formatter: (val) => {
+            return val
+          },
+          style: {
+            fill: '#ffffff'
+          }
+        },
+        line: {
+          lineWidth: 2, // 设置线的宽度
+          stroke: 'red' // 设置线的颜色
+        }
+      })
+
+      chart.axis('hours', {
+        label: {
+          formatter: (val) => {
+            return val
+          },
+          style: {
+            fill: '#ffffff'
+          }
+        },
+        line: {
+          lineWidth: 2, // 设置线的宽度
+          stroke: 'red' // 设置线的颜色
+        }
+      })
+
+      chart
+        .line()
+        .position('hours*total')
+        .color('type')
+        .shape('smooth')
+        .tooltip('hours*type*total', function (hours, type, total) {
+          return {
+            value: (type === 'electric' ? '用电量 ' : (type === 'gas' ? '用气量 ' : (type === 'water' ? '用水量 ' : '热量 '))) + total + (`${type === 'electric' || type === 'heat' ? ' kw/h' : ' m³'}`)
+          }
+        })
+
+      chart
+        .point()
+        .position('hours*total')
+        .color('type')
+        .shape('circle')
+
+      chart.legend({
+        position: 'top-right',
+        itemName: {
+          formatter: (type) => {
+            return type === 'electric' ? '用电量 ' : (type === 'gas' ? '用气量 ' : (type === 'water' ? '用水量 ' : '热量 '))
+          },
+          style: {
+            fill: '#ffffff'
+          }
+        }
+      })
+
+      chart.render()
+    }
+  },
   computed: {},
   watch: {},
-  mounted () {}
+  mounted () {
+    const data = []
+    for (let i = 0; i < 25; i++) {
+      data.push(...[
+        { hours: i, type: 'electric', total: i < 12 ? 10 * i + 1 : 110 - i * 2 },
+        { hours: i, type: 'gas', total: i < 12 ? 5 * i + 1 : 55 - i * 2 },
+        { hours: i, type: 'water', total: i < 12 ? 8 * i + 1 : 88 - i * 2 },
+        { hours: i, type: 'heat', total: i < 12 ? 12 * i + 1 : 121 - i * 2 }
+      ])
+    }
+    this.chartInit(data)
+  }
 }
 </script>
 
@@ -28,5 +129,12 @@ export default {
       flex: 1;
       padding-left: 0 !important;
       padding-right: 0 !important;
+      .detail {
+        padding: 5px;
+        #monitorCurve {
+          width: 100%;
+          height: 100%;
+        }
+      }
     }
 </style>
